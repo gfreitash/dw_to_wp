9a88d8cb4fde06f99071d4faa2ea74ece43cadb1
Gabriel Freitas <gfreitash@gmail.com>
2022-05-18 17:17:49 -0300

    Foi idealizada a primeira função para tornar o script mais completo. Esta será uma função recursiva que tem como objetivo encontrar todas as subpáginas que são referenciadas a partir dos locais que o usuário escolheu para iniciar a busca.

--- dw_to_wp.php
+++ dw_to_wp.php
@@ -333,3 +333,18 @@ foreach ( $DW_INDEXES as $index ) {
         $created++;
     }
 }
+
+//Função recursiva para encontrar todas as páginas da dokuwiki
+function findWikiPages(array $indexes) {
+    //Verifica se a página já está na lista de posts a serem adicionados no wordpress
+    //Se está, retorna
+
+    //Verifica os links da página,
+        //Se são links da mesma wiki, chama esta função novamente
+        //Se são links externos, torna os links disponíveis
+        //Se são arquivos, obtém todos os arquivos da página e os torna disponíveis
+
+    //Por fim, transforma esta página em um post e o adiciona na lista de posts a serem incluidos no wordpress
+
+    //Retorna
+}
